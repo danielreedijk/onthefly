f2485fe5a9a0381c3ac14674fffaa964d1cf9d8d
Daniel Reedijk <d277058@edu.rocwb.nl>
2020-02-03 14:42:52 +0100

    init

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Homescreen</title>
+    <link href="stylesheetairline.css" rel="stylesheet">
+</head>
+<body>
+<ul>
+    <li><a href="home.php">Home</a></li>
+    <li><a href="#"></a></li>
+    <li><a href="#"></a></li>
+</ul>
+</body>
+</html>
